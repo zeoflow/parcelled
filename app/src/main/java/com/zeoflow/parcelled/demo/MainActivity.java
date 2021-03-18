4ff1a40217691c4858f002df283a6eaa3d8e567d
Teodor G <teo@zeoflow.com>
2021-03-18 12:17:58 +0200

    added annotationProcessor

--- app/src/main/java/com/zeoflow/parcelled/demo/MainActivity.java
+++ app/src/main/java/com/zeoflow/parcelled/demo/MainActivity.java
@@ -14,33 +14,15 @@
 
 package com.zeoflow.parcelled.demo;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.zeoflow.app.Activity;
 
-import com.zeoflow.parcelled.demo.model.Address;
 import com.zeoflow.parcelled.demo.model.Person;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
 {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private EditText mAgeEditText;
-    private EditText mNameEditText;
-    private EditText mBdayEditText;
-    private EditText mStreetEditText;
-    private EditText mPostcodeEditText;
-    private EditText mCityEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -48,44 +30,15 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNameEditText = findViewById(R.id.fullName);
-        mBdayEditText = findViewById(R.id.dateOfBirth);
-        mAgeEditText = findViewById(R.id.age);
-        mStreetEditText = findViewById(R.id.street);
-        mPostcodeEditText = findViewById(R.id.postCode);
-        mCityEditText = findViewById(R.id.city);
-
-        findViewById(R.id.imvAdd).setOnClickListener(new View.OnClickListener()
+        findViewById(R.id.imvAdd).setOnClickListener(view ->
         {
-            @Override
-            public void onClick(View view)
+            Person person = Person.create();
+
+            Intent activityIntent = PersonActivity.createIntent(MainActivity.this, person);
+
+            if (activityIntent != null)
             {
-                @SuppressLint("SimpleDateFormat") SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-                // Create the parcelable object using the creator
-                try
-                {
-                    int age = TextUtils.isEmpty(mAgeEditText.getText()) ? 0 : Integer.parseInt(mAgeEditText.getText().toString());
-                    Date date = TextUtils.isEmpty(mBdayEditText.getText()) ? new Date(System.currentTimeMillis()) : df.parse(mBdayEditText.getText().toString());
-                    Address address = Address.create(
-                            mStreetEditText.getText().toString(),
-                            mPostcodeEditText.getText().toString(),
-                            mCityEditText.getText().toString(),
-                            /* Country */ null);
-                    Person person = Person.create(
-                            mNameEditText.getText().toString(),
-                            "last",
-                            date, age, address);
-
-                    Intent activityIntent = PersonActivity.createIntent(MainActivity.this, person);
-
-                    if (activityIntent != null)
-                    {
-                        MainActivity.this.startActivity(activityIntent);
-                    }
-                } catch (ParseException e)
-                {
-                    Log.e(TAG, "onClick: Error parsing date", e);
-                }
+                MainActivity.this.startActivity(activityIntent);
             }
         });
     }
